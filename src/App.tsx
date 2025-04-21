e30defeb62a8121345d5741b3a09aecd49d45ca9
Passion <oscarpassion36@gmail.com>
2025-04-22 00:24:37 +0800

    feat: add pricing section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/sections/Hero.tsx";
 import Brands from "./components/sections/Brands.tsx";
 import Services from "./components/sections/Services.tsx";
 import AboutUs from "./components/sections/AboutUs.tsx";
+import Pricing from "./components/sections/Pricing.tsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Brands />
         <Services />
         <AboutUs />
+        <Pricing />
     </Layout>
   )
 }
