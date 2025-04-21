5304a0615bebd9b6b94735b23ba9c46daf9d6bcc
Passion <oscarpassion36@gmail.com>
2025-04-21 23:30:12 +0800

    feat: add services section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Layout from "./components/Layout.tsx";
 import Hero from "./components/sections/Hero.tsx";
 import Brands from "./components/sections/Brands.tsx";
+import Services from "./components/sections/Services.tsx";
 
 function App() {
   return (
     <Layout title="EdgeAI">
         <Hero />
         <Brands />
+        <Services />
     </Layout>
   )
 }
---
+++ src/components/cards/Service.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Paragraph from "../shared/Paragraph.tsx";
+
+interface ServiceProps {
+    title: string;
+    description: string;
+    icon: React.ReactNode;
+}
+
+const Service = ({ title, description, icon }: ServiceProps) => {
+    return (
+        <div className="p-5 sm:p-6 lg:p-8 rounded-3xl border border-bo-border bg-box-bg shadow-lg
+                        shadow-box-shadow relative overflow-hidden">
+            <div className="rounded-xl bg-gray-300 dark:bg-gray-950 p-3 text-heading-1 w-max relative">{ icon }</div>
+            <div className="mt-16 space-y-4 relative">
+                <h2 className="text-lg md:text-xl font-semibold text-heading-2">{title}</h2>
+                <Paragraph>{description}</Paragraph>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
