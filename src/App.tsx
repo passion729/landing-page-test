f48b4f13975da0e2be5b41cb6760486fb009df86
Passion <oscarpassion36@gmail.com>
2025-04-21 23:10:16 +0800

    feat: add brands section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Layout from "./components/Layout.tsx";
 import Hero from "./components/sections/Hero.tsx";
+import Brands from "./components/sections/Brands.tsx";
 
 function App() {
   return (
     <Layout title="EdgeAI">
         <Hero />
+        <Brands />
     </Layout>
   )
 }
